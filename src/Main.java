eb8b1a43ec4d9169af75cb9f84a87c69630483bb
mameevdi <mameevs@gmail.com>
2022-12-09 00:16:42 +0300

    Домашка выполнена!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task5();
+        task6();
+        task7();
+    }
+
+    public static void task5() {
+        System.out.println("Задача 1"); //Напишите программу, которая выводит в консоль все високосные года, начиная с 1904 года до 2096
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i+" год является високосным");
+        }
+        System.out.println();
+    }
+    public static void task6() {
+        System.out.println("Задача 2"); //Напишите программу, которая выводит в консоль последовательность чисел: 7 14 21 28 35 42 49 56 63 70 77 84 91 98
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+    public static void task7() {
+        System.out.println("Задача 3"); //Напишите программу, которая выводит в консоль последовательность чисел: 1 2 4 8 16 32 64 128 256 512
+        for (int i = 1; i <= 512; i=i*2) {
+            System.out.println(i);
+        }
     }
 }
